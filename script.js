1aa62ed7badfdd712f793721ef9b84eba9dd5ae8
FabianJGL <125667028+FabianJGL@users.noreply.github.com>
2023-04-19 07:35:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function suma() {
+    var num1 = prompt("Ingrese el primer número");
+    var num2 = prompt("Ingrese el segundo número");
+    var resultado = parseFloat(num1) + parseFloat(num2);
+    alert("El resultado de la suma es: " + resultado);
+}
+function resta()
+{
+    var num1 = prompt("Ingrese el primer número");
+    var num2 = prompt("Ingrese el segundo número");
+    var resultado = parseFloat(num1) - parseFloat(num2);
+    alert("El resultado de la resta es: " + resultado);
+}
+function multiplicacion()
+{
+    var num1 = prompt("Ingrese el primer número");
+    var num2 = prompt("Ingrese el segundo número");
+    var resultado = parseFloat(num1) * parseFloat(num2);
+    alert("El resultado de la multiplicación es: " + resultado);
+}
+function division()
+{
+    var num1 = prompt("Ingrese el primer número");
+    var num2 = prompt("Ingrese el segundo número");
+    var resultado = parseFloat(num1) / parseFloat(num2);
+    alert("El resultado de la división es: " + resultado);
+}
